601db29c4ce0edb5812975976fab204e93b06e3f
laijunbin <xyz607xx@gmail.com>
2019-04-24 20:02:50 +0800

    init lesson1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Lesson1Component } from './lesson1/lesson1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Lesson1Component
   ],
   imports: [
     BrowserModule,
