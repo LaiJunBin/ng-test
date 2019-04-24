b9d48de8f653c69a1885c7e5087152058225d25d
laijunbin <xyz607xx@gmail.com>
2019-04-24 20:54:42 +0800

    lesson1 success

--- src/app/lesson1/lesson1.component.ts
+++ src/app/lesson1/lesson1.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Lesson1Component implements OnInit {
 
+  name = 'Lai';
+  url = 'https://google.com';
+  nt = 0;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  sayHello() {
+    alert('Hello ' + this.name);
   }
 
 }
