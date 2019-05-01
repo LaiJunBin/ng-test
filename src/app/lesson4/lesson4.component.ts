e1979e24d38e5d5d182c0cb385694118204ae31a
laijunbin <xyz607xx@gmail.com>
2019-05-01 20:46:12 +0800

    lesson4 success

--- src/app/lesson4/lesson4.component.ts
+++ src/app/lesson4/lesson4.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lesson4.component.css']
 })
 export class Lesson4Component implements OnInit {
+  obj = {
+    a: 5,
+    b: 10
+  };
+  now = new Date();
+
+  account: Account = {
+    account: 'abc',
+    password: 'pass'
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+export class Account {
+  account: string;
+  password: string;
+  email?: string;
 }
