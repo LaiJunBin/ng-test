be415695ad9c78daf0adf77dbc2f3b1783ff10aa
laijunbin <xyz607xx@gmail.com>
2019-05-08 21:16:35 +0800

    get api

--- src/app/abc/abc.component.ts
+++ src/app/abc/abc.component.ts
@@ -1,3 +1,5 @@
+import { User } from './../model/user.model';
+import { HttpService } from './../http/http.service';
 import { BmiService } from './../bmi.service';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
@@ -9,11 +11,25 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class AbcComponent implements OnInit {
 
   @Output() abcButtonClick: EventEmitter<string> = new EventEmitter<string>();
-  
-  constructor(private bmiService: BmiService) { }
+
+  constructor(
+    private bmiService: BmiService,
+    private httpService: HttpService
+  ) { }
 
   ngOnInit() {
     console.log(this.bmiService.calBMI(123, 45));
+    this.httpService.getApi<User[]>('http://localhost/ng_test_api/users').subscribe(
+      data => {
+        console.log(data);
+      },
+      err => {
+        console.log(err);
+      },
+      () => {
+        console.log('finish');
+      }
+    )
   }
 
   abcClick() {
